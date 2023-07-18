a14b4f5930b43a14af421044d7b1ce2beef07611
Manu <manusvathgurudath@gmail.com>
2023-07-18 19:52:54 +0000

    W

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Navbar } from "./navbar";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <div className="container">
         {/* Banner */}
-        <div className="banner">
+        <div className="banner box">
           <div className="banner__content">
             <h1 className="font-heading text-4xl">The Steamology Project</h1>
             <p className="font-body text-lg">
@@ -17,7 +22,7 @@ export default function Home() {
         {/*  */}
 
         {/* Grid */}
-        <div className="flex mt-3 gap-2">
+        <div className="flex mt-3 gap-2 cursor hover:bg-gray-100">
           <div className="box"></div>
           <div className="box">
             <h1 className="font-heading text-2xl mb-10">Our vision</h1>
@@ -25,7 +30,10 @@ export default function Home() {
         </div>
 
         {/* Hear more from our founders */}
-        <div className="flex mt-3 gap-5 items-center">
+        <div
+          className="flex my-10 gap-5 items-center"
+          onClick={() => router.push("/about")}
+        >
           <div className="shrink-0 h-20 w-20 bg-gray-300 rounded-full"></div>
           <div>
             <h1 className="text-uppercase">Hear more from our founders</h1>
