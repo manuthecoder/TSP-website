16a992383c6a0cf4d4be5c1aaf648dcfe9165ea3
_manu.codes <manusvathgurudath@gmail.com>
2023-07-28 13:54:50 -0700

    Update navbar.tsx

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
       { name: "Home", href: "/" },
       { name: "Magazine", href: "/magazine" },
       { name: "Blog", href: "/blog" },
-      { name: "Start a chapter", href: "/start-a-chapter" },
+      { name: "Start a chapter", href: "https://forms.gle/CyhVa3TCHqi3aBTu8" },
       { name: "About us", href: "/about" },
       { name: "Donate now", href: "/donate-now" },
     ].map(({ name, href }) => (
