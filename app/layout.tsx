0e860c87276bb7c8e804fc71a5c773b01a96fc83
_manu.codes <manusvathgurudath@gmail.com>
2023-08-01 18:46:10 +0000

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="shortcut icon" href="/logo.svg" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
