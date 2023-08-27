435ce01c22d7b46d33b707cff1f1a0873b3db465
Manu G <manusvathgurudath@gmail.com>
2023-08-27 04:32:01 +0000

    W

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -9,10 +10,7 @@ export default function Home() {
     <>
       <div className="container">
         {/* Banner */}
-        <div
-          className="banner box border-none h-auto px-2 mb-10"
-          style={{ background: "transparent" }}
-        >
+        <div className="banner box overflow-hidden h-auto px-5 mb-10 to-gray-100 from-gray-100 relative h-[300px]">
           <div className="banner__content">
             <h1 className="font-heading text-4xl font-[700]">
               The Steamology Project
@@ -21,13 +19,21 @@ export default function Home() {
               A community of students and educators who are passionate about
               STEAM.
             </p>
+            <Image
+              src="/logo.svg"
+              width={300}
+              height={300}
+              className="absolute bottom-0 right-0 mr-5 animate-spin opacity-60"
+              style={{ marginBottom: "-120px", animationDuration: "60s" }}
+              alt="Logo"
+            />
           </div>
         </div>
         {/*  */}
 
         {/* Grid */}
         <div className="flex mt-3 gap-2 flex-col sm:flex-row">
-          <div className="box"></div>
+          <div className="box">(placeholder image)</div>
           <div className="box">
             <h1 className="font-heading text-2xl mb-1">Our vision</h1>
             <p>
