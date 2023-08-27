bfe8fa23eb7ec6431b8e5a0173784a297aad453d
Manu G <manusvathgurudath@gmail.com>
2023-08-27 03:13:13 +0000

    Add under construction indicator

---
+++ app/donate-now/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="text-center h-[100vh] flex items-center flex-col justify-center">
+      <h1 className="text-4xl mb-1 font-heading font-[700]">
+        Under construction!
+      </h1>
+      <h1 className="text-lg">
+        We&apos;re working hard to grow our project. Check back later!
+      </h1>
+    </div>
+  );
+}
